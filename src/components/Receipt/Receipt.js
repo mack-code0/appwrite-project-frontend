7a57e6bceabf0bcb7be5f86d8965bccd27d4a2da
mack-code0 <macaulayhilbert202@gmail.com>
2022-04-30 11:35:15 +0800

    updated receipts

--- src/components/Receipt/Receipt.js
+++ src/components/Receipt/Receipt.js
@@ -37,7 +37,7 @@ const Receipt = ({ products, totalPrice }) => {
                 <button onClick={saveReceiptHandler} className="save py-2 px-3">Save</button>
             </div>
             {/* <ReceiptTest products={products} totalPrice={totalPrice} /> */}
-            <Receipt2 />
+            <Receipt1 />
         </div>
     )
 }
--- src/components/Receipt/Receipt1/Product.js
+++ src/components/Receipt/Receipt1/Product.js
@@ -1,11 +1,9 @@
 const Product = () => {
     return (
         <div class="row mb-2 mb-sm-0 py-25 bgc-default-l4">
-            <div class="d-none d-sm-block col-1">4</div>
-            <div class="col-9 col-sm-5">Consulting</div>
-            <div class="d-none d-sm-block col-2">1 Year</div>
-            <div class="d-none d-sm-block col-2 text-95">$500</div>
-            <div class="col-2 text-secondary-d2">$500</div>
+            <div class="col-8 col-sm-8">Consulting</div>
+            <div class="col-2 col-sm-2">1 Year</div>
+            <div class="col-2 col-sm-2 text-secondary-d2">$500</div>
         </div>
     )
 }
